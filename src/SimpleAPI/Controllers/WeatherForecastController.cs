475945cf84b60766895a7262be69f7ad77785ab0
David Salcedo <dvsalcedov@gmail.com>
2021-08-18 21:28:21 -0500

    Return old value on controller

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -34,7 +34,7 @@ namespace SimpleAPI.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 56),
+                TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
